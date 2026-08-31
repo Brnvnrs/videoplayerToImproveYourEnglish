efa85fb3df2aabe760b93760bb6f6dc16ae74ccc
brian <ivan.rios2010@gmail.com>
2026-08-31 11:17:15 -0300

    main.js is in progres

--- main.js
+++ main.js
@@ -1,13 +1,19 @@
 class Main{
     subtitles;
-    definitionofWords;
+    listOfWords;
     constructor(sbt){
         this.subtitles = required("fs").readFileSync(sbt+".srt", "utf8");
+        this.listOfWords = {};
     }
     /* word => meaning */
-    #getMeaning(word){ //
-        
-        
+    #getWords(){ //
+        /* */
+        this.listOfWords = this.subtitles.split(/\s+/);// I get every word or symbol between spaces
+        /*we have to quit the number here */
+        this.listOfWords = filterNumber(this.listOfWords); //the numbers gonna be [0,1,2,3,4,5,6,7,8,9]
+        this.listOfWords = filterSymbol(this.listOfWords); //the symbols gonna be [-,>]
+
+
     }
 
 
