c12b1d028cc71ec5c2480e753878197895e06fe5
brian <ivan.rios2010@gmail.com>
2026-08-31 00:43:01 -0300

    *I need to find a website where i can dowload the video and the subtitles  - Done: I find www.opensubtitles.org
    *I have to find a library which I can find the meaining from the words - Done I'm gonna use this API: https://dictionaryapi.dev/
    and I'm doing the main class, everything is explain in the kanban.txt

---
+++ main.js
@@ -0,0 +1,14 @@
+class Main{
+    subtitles;
+    definitionofWords;
+    constructor(sbt){
+        this.subtitles = required("fs").readFileSync(sbt+".srt", "utf8");
+    }
+    /* word => meaning */
+    #getMeaning(word){ //
+        
+        
+    }
+
+
+}
